54a1fe0b3f061e4f2ef8d739cde1cfa3a4aeadce
Kamish666 <dmalofei2003@gmail.com>
2024-10-21 23:19:28 +0300

    Добавлен класс ChangeColor
    При нажатии правой/левой кнопки мыши одни платформы появляются, другие пропадают

---
+++ Platformer/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    private int _currentColorIndex = 0; // 0: Зеленый, 1: Синий, 2: Красный
+
+    private GameObject[] _colorBlocs;
+
+    private void Start()
+    {
+        _colorBlocs = new GameObject[] { GameObject.Find("GreenBlocks"), GameObject.Find("BlueBlocks"), GameObject.Find("RedBlocks") };
+
+        UpdateColorVisibility();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Левая кнопка мыши
+        {
+            ChangeColorIndex(1);
+        }
+
+        if (Input.GetMouseButtonDown(1)) // Правая кнопка мыши
+        {
+            ChangeColorIndex(-1);
+        }
+    }
+
+    private void ChangeColorIndex(int number)
+    {
+        _currentColorIndex = (_currentColorIndex + number + _colorBlocs.Length) % _colorBlocs.Length;
+        UpdateColorVisibility();
+    }
+
+    private void UpdateColorVisibility()
+    {
+        for (int i = 0; i < _colorBlocs.Length; i++)
+        {
+            _colorBlocs[i].SetActive(i == _currentColorIndex);
+        }
+    }
+}
