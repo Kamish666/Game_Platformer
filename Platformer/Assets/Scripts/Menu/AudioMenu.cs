3b82ea503bd084ab113308a44aba2226c2edfdf6
Kamish666 <dmalofei2003@gmail.com>
2024-11-21 16:02:03 +0300

    Добавлена регулировка музыки и звуков

---
+++ Platformer/Assets/Scripts/Menu/AudioMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioMenu : MonoBehaviour
+{
+    [SerializeField] private Slider _musicSlider, _soundSlider;
+
+    private string _musicKey = "MusicVolume", _soundKey = "SoundVolume";
+
+    public delegate void Audio(int volume);
+    public event Audio OnChangeMusic;
+    public event Audio OnChangeSound;
+
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey(_musicKey))
+            PlayerPrefs.SetInt(_musicKey, 3);
+        if (!PlayerPrefs.HasKey(_soundKey))
+            PlayerPrefs.SetInt(_soundKey, 8);
+
+        _musicSlider.value = PlayerPrefs.GetInt(_musicKey);
+        _soundSlider.value = PlayerPrefs.GetInt(_soundKey);
+
+        // Подписываемся на события изменения значений слайдеров
+        _musicSlider.onValueChanged.AddListener(OnMusicVolumeChange);
+        _soundSlider.onValueChanged.AddListener(OnSoundVolumeChange);
+    }
+
+    // Метод для обработки изменения громкости музыки
+    private void OnMusicVolumeChange(float value)
+    {
+        PlayerPrefs.SetInt(_musicKey, (int)value);
+        PlayerPrefs.Save();
+        OnChangeMusic?.Invoke((int)value);
+        Debug.Log("Music Volume changed to: " + value);
+    }
+
+    // Метод для обработки изменения громкости звуковых эффектов
+    private void OnSoundVolumeChange(float value)
+    {
+        PlayerPrefs.SetInt(_soundKey, (int)value);
+        PlayerPrefs.Save(); 
+        OnChangeSound?.Invoke((int)value);
+        Debug.Log("Sound Volume changed to: " + value);
+    }
+
+    private void OnDestroy()
+    {
+        // Отписываемся от событий, чтобы избежать утечек памяти
+        _musicSlider.onValueChanged.RemoveListener(OnMusicVolumeChange);
+        _soundSlider.onValueChanged.RemoveListener(OnSoundVolumeChange);
+    }
+}
---
+++ Platformer/Assets/Scripts/Settings/AudioSettings.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+    [SerializeField] private AudioSource _musicSource, _soundSource;
+
+    [SerializeField] private int _maxVolumeLivel = 10;
+
+    private string _musicKey = "MusicVolume", _soundKey = "SoundVolume";
+
+    private void Start()
+    {
+        AudioMenu menu = GetComponent<AudioMenu>();
+        if (menu != null)
+        {
+            menu.OnChangeMusic += ChangeVolumeMusic;
+            menu.OnChangeSound += ChangeVolumeSound;
+        }
+
+
+        _musicSource.volume = CalculateVolume(PlayerPrefs.GetInt(_musicKey));
+        _soundSource.volume = CalculateVolume(PlayerPrefs.GetInt(_soundKey));
+    }
+
+    private void ChangeVolumeMusic(int volume) =>
+        _musicSource.volume = CalculateVolume(PlayerPrefs.GetInt(_musicKey));
+
+    private void ChangeVolumeSound(int volume) =>
+        _soundSource.volume = CalculateVolume(PlayerPrefs.GetInt(_soundKey));
+
+    private float CalculateVolume(int volume) 
+        => (float)volume / _maxVolumeLivel;
+}
