781243fbffad8e57cd68a128f323d6d2dfb6285d
Kamish666 <dmalofei2003@gmail.com>
2025-03-07 09:14:16 +0300

    Создана прыгающая ловушка

---
+++ Platformer/Assets/Scripts/Enemy/JumpingTrap.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingTrap : MonoBehaviour
+{
+    [SerializeField] private Animation _jamp;
+
+    [SerializeField] private float _strenght;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var player = collision.gameObject.GetComponent<PlayerMovement>();
+        if (player != null)
+        {
+            player.GetComponent<Rigidbody2D>().AddForce(transform.up * _strenght, ForceMode2D.Impulse);
+        }
+    }
+
+
+}
