85db01819b2fe613b51eb6a93df3cd212ac55cf8
Kamish666 <dmalofei2003@gmail.com>
2025-05-10 01:33:40 +0300

    создал постоянную систему частиц для игрока

---
+++ Platformer/Assets/Scripts/ParticleSystemColors.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemColors : MonoBehaviour
+{
+    [SerializeField] private Material redMaterial;
+    [SerializeField] private Material greenMaterial;
+    [SerializeField] private Material blueMaterial;
+
+    [SerializeField] private Gradient redGradient;
+    [SerializeField] private Gradient greenGradient;
+    [SerializeField] private Gradient blueGradient;
+
+    private ParticleSystem _particleSystem;
+    private ParticleSystem.ColorOverLifetimeModule _colorOverLifetime;
+    private ParticleSystemRenderer _renderer;
+
+    private void Awake()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+        _colorOverLifetime = _particleSystem.colorOverLifetime;
+        _renderer = GetComponent<ParticleSystemRenderer>();
+
+        ChangeColor changeColorScript = ChangeColor.instance;
+        if (changeColorScript != null)
+        {
+            changeColorScript.enemyColors += OnColorChanged;
+        }
+        else
+        {
+            Debug.LogWarning("ChangeColor script not found in the scene");
+        }
+    }
+
+    private void OnColorChanged(bool red, bool green, bool blue)
+    {
+        if (red)
+        {
+            ApplyMaterialAndGradient(redMaterial, redGradient);
+        }
+        else if (green)
+        {
+            ApplyMaterialAndGradient(greenMaterial, greenGradient);
+        }
+        else
+        {
+            ApplyMaterialAndGradient(blueMaterial, blueGradient);
+        }
+    }
+
+    private void ApplyMaterialAndGradient(Material material, Gradient gradient)
+    {
+        //_renderer.material = material;
+        _colorOverLifetime.enabled = true;
+        _colorOverLifetime.color = new ParticleSystem.MinMaxGradient(gradient);
+    }
+}
